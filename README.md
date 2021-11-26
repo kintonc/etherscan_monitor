cafee4d9eb007593a811ce47e99a1217c1d50d29
kinton <kinton114@gmail.com>
2021-11-25 22:34:00 -0500

    v0.2 release

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Etherscan wallet monitor
+# Etherscan and AVAX Snowtrace wallet monitor
 
-Simple tool to monitor Ethereum wallets for new transactions. Uses [Etherscan](https://etherscan.io).
+Simple tool to monitor Ethereum and Avalanche wallets for new transactions. Uses [Etherscan](https://etherscan.io) and [Snowtrace](https://snowtrace.io). Can be easily extended to support Fantom ([FTMScan](https://ftmscan.com)), Binance ([BSCScan](https://bscscan.com)), Polygon ([Polygonscan](https://polygonscan.com)), Arbitrum([Arbiscan](https://arbiscan.io)), Moonriver ([Moonscan](https://moonriver.moonscan.io)), and more chains.
 
-Script checks for new transactions every 60 secs (this can be changed easily).
+Script checks for new transactions every 60 secs (this can be changed easily by setting the `CHECK_FREQUENCY_SECONDS` variable).
 
 To use:
 
-1. Download repo
-2. Go to [etherscan.org and get your API key](https://etherscan.io/apis)
-3. Go to `config_sample.yaml`, and add [1] your API key and [2] the address and a name (used for identifying addresses) for the wallets you want to monitor.
+1. Download repo. Install playsound v 1.2.2 python library (`pip install playsound==1.2.2`)  
+2. Go to [etherscan.io](https://etherscan.io/apis) and [snowtrace.io](https://snowtrace.io/apis), and get your API key]
+3. Go to `config_sample.yaml`, and add [1] your API keys and [2] for every wallet you want to monitor, enter the address, a name (used for identifying addresses), and the blockchain `networks` you want to monitor transactions for this wallet.
 4. Rename `config_sample.yaml` to `config.yaml`
 5. Run `monitor.py`. Script will continuously monitor until it is quit.
+
+Acceptable network parameters (`network`) for YAML file:
+* the network value must be a comma-separated array 
+  * Example 1: `['eth', 'avax']`
+  * Example 2: `['eth']`
+* Can be one or more of `eth`, `avax`
--- monitor.py
+++ monitor.py
@@ -1,4 +1,10 @@
-import os, requests, json, pickle, time, datetime, playsound, multiprocessing, logging, yaml
+import os, requests, json, pickle, time, datetime, playsound, logging, yaml, threading
+
+#####
+# Parameters
+CHECK_FREQUENCY_SECONDS = 60
+
+#####
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,11 +19,14 @@ def load_config(filepath):
     return conf
 
 def play_notification():
-    playsound.playsound('notification-positive.wav')
+    playsound.playsound('notif.mp3')
 
-def pull_api(address, api_key):
+def pull_api(address, api_key, network):
 
-    r = requests.get('https://api.etherscan.io/api?module=account&action=txlist&address=' + address + '&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=' + api_key)
+    if network == 'eth':
+        r = requests.get('https://api.etherscan.io/api?module=account&action=txlist&address=' + address + '&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=' + api_key)
+    elif network == 'avax':
+        r = requests.get('https://api.snowtrace.io/api?module=account&action=txlist&address=' + address + '&startblock=1&endblock=99999999&page=1&offset=10&sort=desc&apikey=' + api_key)
 
     response = json.loads(r.text)
     return response
@@ -46,45 +55,73 @@ def compare_txns(old, new):
         return True
     return False
 
-def check_new_txn(user, api_key):
+def check_new_txn(user, api_keys_all_networks, notif_played_count):
     readable_name = user['name']
     address = user['address']
+    networks = user['network']
 
-    filename = 'txn/' + address
+    for network in networks:
+        filename = 'txn/' + address + '_' + network
 
-    # Pull latest txn from file
-    file_latest_txn = load_latest_txn(filename)
+        # Pull latest txn from file
+        file_latest_txn = load_latest_txn(filename)
 
-    # Pull latest TXN  from API
-    response = pull_api(address, api_key)
+        api_key = api_keys_all_networks[network]
+        # Pull latest TXN  from API
+        response = pull_api(address, api_key, network)
+        
+        if 'result' in response:
+            if len(response['result']) >= 1:
+                response_latest_txn = response['result'][0]
+
+        # If the API response doesn't have a response['result'][0], then response_latest_txn = False
+        if 'response_latest_txn' not in locals():
+            response_latest_txn = False
+
+        if response_latest_txn != False:
+            # If the response latest txn is different from file latest txn, then sound notification
+            response_latest_txn['confirmations'] = 0 # Set confirmations to 0 for easy comparison
+            if response_latest_txn != file_latest_txn:
+                if notif_played_count == 0:
+                    # start_time_notif = datetime.datetime.now() // FOR PERFORMANCE DEBUGGING
+                    thr = threading.Thread(target=play_notification,args=(),kwargs={})
+                    thr.start()
+                    # we don't want to play notif sound more than once, every time data is pulled from API even for muiltiple accounts
+                    # so we set notif_palyed_count = 1
+                    notif_played_count = 1 
+                    # end_time_notif = datetime.datetime.now() // FOR PERFORMANCE DEBUGGING
+                    # print((end_time_notif-start_time_notif).total_seconds()) // FOR PERFORMANCE DEBUGGING
+                    
+                if network == 'eth':
+                    txn_url = f'https://etherscan.io/tx/{response_latest_txn["hash"]}'
+                elif network == 'avax':
+                    txn_url = f'https://snowtrace.io/tx/{response_latest_txn["hash"]}'
+
+                logging.info(f'{readable_name}_{network} - new txn - {txn_url}')
+                save_latest_txn(response_latest_txn, filename)
     
-    if 'result' in response:
-        if len(response['result']) >= 1:
-            response_latest_txn = response['result'][0]
-
-    # If the API response doesn't have a response['result'][0], then response_latest_txn = False
-    if 'response_latest_txn' not in locals():
-        response_latest_txn = False
-
-    if response_latest_txn != False:
-        # If the response latest txn is different from file latest txn, then sound notification
-        response_latest_txn['confirmations'] = 0 # Set confirmations to 0 for easy comparison
-        if response_latest_txn != file_latest_txn:
-            #play_notification()
-            logging.info(f'{readable_name} - new txn - https://etherscan.io/tx/{response_latest_txn["hash"]}')
-            save_latest_txn(response_latest_txn, filename)
+    return notif_played_count
 
 if __name__ == '__main__':
     # conf means config
     conf = load_config('config.yaml')
-    etherscan_api_key = conf['etherscan_api_key']
+    api_key = {}
+    api_key['eth'] = conf['etherscan_api_key']
+    api_key['avax'] = conf['snowtrace_api_key']
     users = conf['users']
 
     while True:
+        start_time = datetime.datetime.now()
+        notif_played_count = 0
+
         for key, user in users.items():
-            check_new_txn(user, etherscan_api_key)
-        print('Last checked: ' + datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S') + ', ' + str(len(users)) + ' users',end = '\r')
-        time.sleep(60)
+            notif_played_count = check_new_txn(user, api_key, notif_played_count)
+        
+        end_time = datetime.datetime.now()
+        time_elapsed = (end_time - start_time).total_seconds() 
+
+        print('Last checked: ' + datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S') + ', ' + str(len(users)) + ' users, ' + str(time_elapsed) + ' s',end = '\r')
+        time.sleep(CHECK_FREQUENCY_SECONDS)
 
     pool = multiprocessing.Pool(2)
     pool.map(check_new_txn, users) 
